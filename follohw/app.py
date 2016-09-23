6e8b68831564b9bf653652fe09fb7864b8280277
canibal <canaan@thetomato.co>
2016-09-22 23:23:44 -0400

    messing with client-side auth

--- follohw/app.py
+++ follohw/app.py
@@ -11,10 +11,13 @@ render = web.template.render('templates/', base="layout")
 
 class Index(object):
     def GET(self):
-        form = web.input(code=None)
-        access_token = "%s" % form.code
+        form = web.input(access_token=None)
+        access_token = "%s" % form.access_token
 
-        return render.login(access_token = access_token)
+        if access_token:
+            return render.index(access_token = access_token)
+        else:
+            return render.login()
 
     def POST(self):
         form = web.input(username="", password="")
