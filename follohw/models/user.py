d8f65149c7acd210e41fe50e82036b8de63a6c05
canibal <canaan@thetomato.co>
2016-09-28 05:04:37 -0400

    fix user.py

---
+++ follohw/models/user.py
@@ -0,0 +1,15 @@
+from urllib import urlopen
+import json
+
+class User(object):
+
+
+    def __init__(self):
+        self.title = "This is my title"
+        self.content = "this is my content"
+        self.author = "I am the author"
+
+
+
+    def get_username():
+        pass
