481ba41fb76ea6f8a39314f73ce540f92d9e1a43
canibal <canaan@thetomato.co>
2016-09-28 05:18:43 -0400

    things

--- follohw/models/user.py
+++ follohw/models/user.py
@@ -7,9 +7,8 @@ class User(object):
     def __init__(self):
         self.title = "This is my title"
         self.content = "this is my content"
-        self.author = "I am the author"
+        self.author = "Canaan F."
 
 
-
-    def get_username():
+    def get_user_id(self, ):
         pass
